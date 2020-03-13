41c24d9db0cd6b3e81c1da739c8e82fb897cb08c
AndriiShnurkov <Shans12.84@ukr.net>
2020-03-13 13:39:46 +0200

    closure and context with classWork

--- closure/index.js
+++ closure/index.js
@@ -8,26 +8,46 @@
 // const newResult = getMult(12);
 // newResult();
 
-// 2 ============
-// const counter = function (n = 0) {
-//     let count = n;
-//     return function (z = 1) {
-//         count += z;
-//         return console.log('count', count);
+// =================================
+// const getMult = function () {
+//     let n = 0;
+//     return function () {
+//         return console.log('result:', n += 1);
 //     }
 // }
-// const counter1 = counter();
-// counter1()
-// counter1()
-// counter1()
-// counter1()
-// const counter2 = counter(10);
-// counter2()
-// counter2()
-// counter2()
-// counter2()
-// const counter3 = counter();
-// counter3(2)
-// counter3(2)
-// counter3(2)
-// counter3(2)
+// const newResult = getMult();
+// newResult();
+// newResult();
+// newResult();
+
+// 2 ============
+const counter = function (n = 0) {
+    let count = n;
+
+    return function (step = 1) {
+        count += step;
+        return console.log('count', count);
+    }
+
+}
+const counter1 = counter(50);
+counter1(50)
+counter1(2)
+counter1(2)
+counter1(2)
+console.log("================");
+
+const counter2 = counter();
+counter2()
+counter2()
+counter2()
+counter2()
+console.log("================");
+
+const counter3 = counter();
+counter3(2)
+counter3(2)
+counter3(2)
+counter3(2)
+
+
--- context/index.js
+++ context/index.js
@@ -1,8 +1,9 @@
 // 1 ================
+// "use strict"
 // const getContext = function () {
 //     console.log('current Context :', this);
 // }
-// getContext(); //window
+// getContext(); //window //undefined
 
 // 2 ===============
 // const getContext = function () {
@@ -10,27 +11,50 @@
 // }
 // const object = {
 //     context: "object",
-//     getThisContext: getContext
+//     getContext,
 // }
-// object.getThisContext() //object
+// object.getContext() //object
 
 // 3 ====== bind ===========
 // const logInfo = function () {
 //     console.log('this.name :', this.name);
 //     console.log('this.status :', this.status);
-//     console.log('this.students :', this.students);
+//     console.log('this.course :', this.course);
 // }
-// const person = {
+
+// const students = [{
+//     id: 'vfgndshmfshdvfhmsdfbmhdsfsdsc',
 //     name: "Andrii",
 //     status: "teacher",
-//     students: "bootCamp 19"
+//     course: "bootCamp 19",
+// }, {
+//     id: 'gfdfgfdgdf',
+//     name: "Maxim",
+//     status: "student",
+//     course: "bootCamp 19",
+// }, {
+//     id: 'vfgndshmfshdvfhmdfgdfgdfgsdfbmhdsfsdsc',
+//     name: "Nikita",
+//     status: "lazy student",
+//     course: "bootCamp 19",
+// }]
+
+// for (const student of students) {
+//     // logInfo.call(student);
+//     // (student.course === "bootCamp 19") && console.log('student.name :', student.name);
+//     (student.id === 'gfdfgfdgdf') ? console.log('student.name :', student.name) : console.log("not exist");
+//     // ((student.status === "student") || (student.status === "lazy student")) && console.log('student.name :', student.name);
 // }
-// const getInfo = logInfo.bind(person);
-// getInfo();
-// // === alternative expression with bind ===
-// // logInfo.bind(person)()
+
+
+// logInfo.call(person);
+
+// === alternative expression with bind ===
+// logInfo.bind(person)()
 
 // 4 ======== bind ===============
+// 'use strict';
+
 // const logInfo = function () {
 //     console.log("name:", this.name);
 //     console.log("status:", this.status);
@@ -46,23 +70,57 @@
 //     status: "student",
 //     course: "bootCamp 19"
 // }
-// logInfo.call(andrii);
-// logInfo.call(maxim);
+
+// logInfo.call(andrii)
+// // logInfo.bind(andrii)();
+
+// // const logger = logInfo.bind(andrii);
+// // logger();
+
+// // logInfo.call(maxim);
+
+// ==================
+// const logInfo = function (status) {
+//     this.newStatus = status;
+//     console.log('this :', this);
+// }
+// const andrii = {
+//     name: "Andrii",
+//     status: "teacher",
+//     course: "bootCamp 19"
+// }
+// logInfo.call(andrii, 'mentor')
 
 // 5 =======================
 // const user = {
-//     userName: "default",
-//     logInfo: function (userName, status, course) {
-//         console.log('userName :', userName);
-//         console.log('status :', status);
-//         console.log('course :', course);
+//     userName: "castom name",
+//     logInfo: function (userName1111111111, status, course) {
+//         // console.log('userName :', this.userName);
+//         this.userName = userName1111111111
+//         // console.log('userName :', userName1111111111);
+//         // console.log('status :', status);
+//         // console.log('course :', course);
+//         console.log('this :', this);
 //     }
 // }
 // user.logInfo('Nikita', 'student', "bootCamp 19")
 
+// ======================
+// const user = {
+//     userName: "castom name",
+//     logInfo: function (userName1111111111) {
+//         this.userName = userName1111111111
+//     }
+// }
+// user.logInfo('Nikita', )
+
 // 6 ====================
 // const user = {
 //     userName: "default",
+//     status: "default",
+//     course: "default",
+
+
 //     logInfo: function () {
 //         console.log('userName :', this.userName);
 //         console.log('status :', this.status);
@@ -74,9 +132,26 @@
 //     status: "student",
 //     course: 'BootCamp 25'
 // }
-// const getInfo = user.logInfo.bind(vlad);
+// const dimonchik = {}
+
+// user.logInfo()
+
+// const getInfo = user.logInfo.bind(dimonchik);
 // getInfo();
+
 // // alternative expression call
 // // user.logInfo.call(vlad)
 
 
+// Practize at home
+// const numbers = [1, 2, 3, 4, 5, 6];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(`${i}`, numbers[i]);
+// }
+// for (const number of numbers) {
+//     console.log(`${numbers.indexOf(number)}`, number);
+// }
+
+
+
+// logger()
